308ac82a604591a99118d60ba2d2246a09b55831
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-01-21 17:40:50 -0300

    Nuevo

--- GameAhorcado/src/main/java/com/mycompany/gameahorcado/GameAhorcado.java
+++ GameAhorcado/src/main/java/com/mycompany/gameahorcado/GameAhorcado.java
@@ -6,7 +6,7 @@ public class GameAhorcado {
 
     public static void main(String[] args) {
 
-
+//* Hola paa Nea */
         Scanner scanner = new Scanner(System.in);
         boolean seguirJugando = true;
 
